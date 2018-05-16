df3e000c3cbae9ecc684e02d191b2f0523cd5217
Unknown <Maverickfox@outlook.com>
2018-05-16 17:37:21 -0400

    Comment Fix

--- TheBestGameEver.py
+++ TheBestGameEver.py
@@ -1,39 +1,10 @@
-#this is where most or all of the code for the game will be contained
-#We'll start by commenting the game and then fill in the rest with code later.
-#open a game window
-#show main menu screen
-        #option to play
-        #option for how to play
-        #option high scores
-        #game options
-            #sound
-            #game speed
-            #start-length
+'''
+Python Snake Game on a Rasbery pi 3
 
-        #create game objects
-            #movable area
-            #score
-            #snake
-            #apples - of different types
-        #set stage for game
-            #place snake
-            #place the apple
-    #main game loop begins
-        #move snake
-        #check for user input and adjust direction accordingly
-        #when you eat an apple
-            #increment score
-            #add length to snake body
-            #place new apple
-        #check for collision with wall or self
-            #if a collision occurs
-                #end main game loop
-    #show game over screen
-    #ask user for name to save high score
-    #display top scores
-    #ask user to play again
+Team Rocket:
+Kyle, Sem, Hansel, Pat
+'''
 
-print("Hello World!")
 import sys, random, pygame, dumbmenu as dm
 from pygame import *	#needed to reference keys directly instead of pygame.K_xxxx
 
